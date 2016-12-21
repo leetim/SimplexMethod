d8be4161f343bbe3f59eafd40d0d7c8ebd4bfa51
Artem Batrakov <leetim@live.ru>
2016-12-21 14:54:31 +1000

    Убрал коменты

--- sm.py
+++ sm.py
@@ -12,23 +12,6 @@ min_first = [np.min(i) for i in A_tild]
 max_first = [np.max(i) for i in np.transpose(A_tild)]
 e6 = np.ones(6)
 e8 = np.ones(8)
-# print np.matrix.I(2, 2)
-# def diag(i ,j):
-#     if i == j:
-#         return 1
-#     else:
-#         return 0
-
-# print diag(1, 2)
-
-# def simplex(A, start_x):
-#     n = len(A);
-#     m = len(A[0]);
-#     e = [[1 for i in range(m)]]
-#     zer = [[0 for i in range(m)]]
-#     I = [[diag(i, j) for j in range(m)] for i in range(n)]
-#     M = np.bmat([[e, zer, [[0]]], [A, I, np.transpose([start_x])]])
-#     print M
 
 T = np.transpose
 M = np.matrix
@@ -49,6 +32,3 @@ Y = Y.x/sum(Y.x)
 print Y
 
 print np.matrix(X)*np.matrix(A)*np.transpose(np.matrix(Y))
-# print sum(X)
-# print e6
-# simplex(A, [0 for i in range(6)])
